96cf4b9ed6e3ed571b9559c9064579173f49be77
Srinivas <chebolu572@gmail.com>
2025-08-25 22:43:52 +0530

    DatatYpes

---
+++ Day3/Test.js
@@ -0,0 +1,17 @@
+// JS is Dynamically Typed Language
+let age=20; // number
+console.log(typeof age); // number
+
+age="Srinivas";// valid
+console.log(typeof age); // string
+
+console.log(age); // Srinivas
+
+//type safety in JS
+
+let message="Hello";
+let count=30;
+message=100; // valid in JS but not a good practice
+
+let result="50"+10; // 5010
+console.log(result); // 5010
---
+++ Day3/Test.ts
@@ -0,0 +1,11 @@
+//TS is Statically Typed Language
+let data:number=20;
+//data="Srinivas"; // Error: Type 'string' is not assignable to type 'number'
+console.log(data); // number
+
+//type safety in JS
+
+let number1:string="100"; // number
+let number2:number=200; // number
+//let result=number1+number2; // number   
+console.log(result); // 300
